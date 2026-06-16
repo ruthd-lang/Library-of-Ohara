5d380fdb130d6e2047bcdc091133231c8f5d2603
ruthedias <ellendiasdasilvaruth@gmail.com>
2026-06-16 19:48:27 -0300

    Implementação de animação com AOS

--- LibraryOhara/src/app/app.ts
+++ LibraryOhara/src/app/app.ts
@@ -1,15 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core'; 
+import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router'; 
+import { filter } from 'rxjs/operators';
+import * as AOS from 'aos'; 
+
 import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 import { Header } from './Components/header/header';
 import { Footer } from './Components/footer/footer';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterModule, RouterOutlet, MdbRippleModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit { 
   protected readonly title = signal('LibraryOhara');
+  
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    AOS.init({
+      duration: 800, 
+      once: true
+    });
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      AOS.refresh();
+    });
+  }
 }
